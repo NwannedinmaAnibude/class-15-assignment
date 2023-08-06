2c470a03522cd2dcacf7c540590f7988f932ecde
NwannedinmaAnibude <134140003+NwannedinmaAnibude@users.noreply.github.com>
2023-08-06 23:08:56 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,22 @@
 # main.tf
 
 provider "aws" {
-  region = "eu-west-2" # Replace this with your desired AWS region
+  region = "eu-west-2"
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "nwanne" # Replace this with your desired bucket name
-  acl    = "private" # You can change the ACL (Access Control List) as per your requirements
+  bucket = "nwanne-bucket"  # Change this to a unique bucket name you want to use
+
+  # Uncomment the following block if you want to enable server-side encryption with AWS managed keys
+  # server_side_encryption_configuration {
+  #   rule {
+  #     apply_server_side_encryption_by_default {
+  #       sse_algorithm = "AES256"
+  #     }
+  #   }
+  # }
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.my_bucket.bucket
 }
