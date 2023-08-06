a3075060fcaf03fa265f5670e270af47a4e3f564
NwannedinmaAnibude <134140003+NwannedinmaAnibude@users.noreply.github.com>
2023-08-07 00:49:28 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
   region = "eu-west-2"  # Change this to your desired AWS region
 }
 
-resource "aws_s3_bucket" "my_bucket" {
+resource "aws_s3_bucket_acl" "my_bucket" {
   bucket = "class15-assignment"  # Change this to your desired bucket name
   acl    = "private"                  # Access Control List for the bucket (options: private, public-read, public-read-write, authenticated-read)
 }
